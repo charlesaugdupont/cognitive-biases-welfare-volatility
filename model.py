prob weighting bug fix

model.py
@@ -246,8 +246,9 @@ def value_iteration_pt_cpt(
     # Rank-dependent cumulative weighting for probabilities
     cpt_P_increase = weighting_function(P_H_increase, gamma)
     cpt_P_increase_complement = 1 - cpt_P_increase
-    cpt_P_decrease_complement = weighting_function(1 - P_H_decrease, gamma)
-    cpt_P_decrease = 1 - cpt_P_decrease_complement
+
+    cpt_P_decrease = weighting_function(P_H_decrease, gamma)
+    cpt_P_decrease_complement = 1 - cpt_P_decrease
 
     w_vals, h_vals = np.arange(1, N + 1), np.arange(1, N + 1)
     W, H = np.meshgrid(w_vals, h_vals, indexing='ij')